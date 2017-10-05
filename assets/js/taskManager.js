b73670f6d8930b3db28023943f49689232dff9ea
Bechar Gherbi <cn.b@outlook.com>
2017-10-05 12:35:09 +0100

    Add revamped addTask function.

--- assets/js/taskManager.js
+++ assets/js/taskManager.js
@@ -1,89 +1,98 @@
+/* eslint no-console: 0 */
 const taskManager = (function() {
+  let tasksStorage;
+  // Default configuration
+  let config = {
+    schemaName: 'tasks',
+    storage: chrome.storage.local,
+    Schema: {},
+    newItemTask: undefined,
+  };
+  // Extract methods from config object.
+  let {schemaName, storage, Schema, newItemTask} = config;
+
   /**
-   * Add a task function.
-   * @param {string} taskText String task item.
-   * @param {object} options configuration.
+   * addTask function: adds task item into chrome storage.
+   * @param {string} taskText valid string task.
    */
-  function addTask(taskText, options) {
-    const defaultOptions = {
-      storageType: 'localStorage',
-      maxTasks: 15,
-      chromeStorageSync: true,
-      errors: {
-        success: 'Task has been added!',
-        failure: 'Couldn\'t save task!',
-        exists: 'Item already exists in storage!',
-      },
+  function addTask(taskText) {
+    // Check if taskText is undefined.
+    if (!taskText) {
+      log('taskText argument maust be a valid string');
+    }
+    // Create JSON schema to use in storage.
+    Schema = {
+      [schemaName]: [
+        {
+          'taskID': taskIDGenerator(),
+          'taskItem': taskTextValidator(taskText),
+        },
+      ],
     };
-    options = defaultOptions || options;
-
-    (function saveTask() {
-      const {storageType, chromeStorageSync} = options;
-      if (typeof storageType === 'string' && storageType !== undefined) {
-        if (storageType === 'localStorage') {
-          addItemWithLocalStorage();
-        } else if (storageType === 'chromeStorage') {
-          if (chromeStorageSync) {
-            addItemWithChromeStorage(true);
-          } else {
-            addItemWithChromeStorage(false);
-          }
-        }
+    // Get whole storage area in chrome storage.
+    storage.get(null, function(storageArea) {
+      // Assign tasks storage area to tasksStorage var.
+      tasksStorage = storageArea[schemaName];
+      // Check if tasks storage area is undefined.
+      if (!tasksStorage) {
+        // Create Schema for first time along with first task item.
+        storage.set(Schema, function() {
+          log('Schema set success!');
+        });
       } else {
-        console.error('storageType must be a string value!');
-      }
-
-      /**
-       * Save Task item using localStorage API.
-       */
-      function addItemWithLocalStorage() {
-        localStorage.setItem(taskIDGenerator(), taskTextValidator(taskText));
-        console.log(options.errors.success);
-      }
-
-      /**
-       * Save Task item using crome.storage extension API.
-       * @param {boolean} sync
-       */
-      function addItemWithChromeStorage(sync) {
-        const taskID = taskIDGenerator();
-        const taskTextValidated = taskTextValidator(taskText);
-        let chromeStorage;
-        if (sync) {
-          chromeStorage = chrome.storage.sync;
-        } else {
-          chromeStorage = chrome.storage;
-        }
-        chromeStorage.set({[taskID]: taskTextValidated}, function() {
-          console.log(options.errors.success);
+        // Get whole tasks storage area in chrome storage.
+        storage.get(schemaName, function(tasksData) {
+          // Create new task item Object.
+          newItemTask = {
+            'taskID': taskIDGenerator(),
+            'taskItem': taskTextValidator(taskText),
+          };
+          // tasksArray: Tasks items array from chrome storage.
+          const tasksArray = tasksData[schemaName];
+          // Push new task item to tasksArray.
+          tasksArray.push(newItemTask);
+          // Save new tasksArray to tasks storage area in chrome storage.
+          storage.set({
+            [schemaName]: tasksArray,
+          }, function() {
+            log('item has been added!');
+          });
         });
       }
-    })();
+    });
+  }
+  /**
+   * Generate a unique ID for every task.
+   * @return {string} 8 numbers long.
+   */
+  function taskIDGenerator() {
+    return Math.random().toString().slice(2, 11);
+  }
 
-    /**
-     * Generate a unique ID for every task.
-     * @return {string} 8 numbers long.
-     */
-    function taskIDGenerator() {
-      return Math.random().toString().slice(2, 11);
+  /**
+   * Task Text string validator.
+   * @param {string} taskText
+   * @return {string} taskText
+   */
+  function taskTextValidator(taskText) {
+    if (typeof taskText === 'string' && taskText !== undefined &&
+      taskText !== '') {
+      return taskText;
     }
+    console.error('taskText: must be string!');
+  }
 
-    /**
-     * Task Text string validator.
-     * @param {string} taskText
-     * @return {string} taskText
-     */
-    function taskTextValidator(taskText) {
-      if (typeof taskText === 'string' && taskText !== undefined &&
-        taskText !== '') {
-        return taskText;
-      }
-      console.error('taskText: must be string!');
-    }
+  /**
+   * console logger.
+   * @param {*} logged
+   */
+  function log(logged) {
+    console.log(logged);
   }
 
   return {
     addTask: addTask,
+    getTasks: getTasks,
   };
 })();
 
