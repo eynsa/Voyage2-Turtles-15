d23674ee82fa2de3e8fb12cb3807b08700e3dcff
Steven Eysermans <31731363+eynsa@users.noreply.github.com>
2017-10-07 10:10:25 +0200

    tabs now work in popup

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -21,6 +21,22 @@ function options() {
 
 /** document init function */
 function init() {
+  const tabs = document.querySelectorAll('li.tab-button');
+  const tabContent = document.querySelectorAll('div.tab-content');
+
+  tabs.forEach(function(element) {
+    element.addEventListener('click', function(event) {
+      let tab = this.dataset.tab;
+      let active = document.querySelector(`div.tab-content[data-tab="${tab}"]`);
+      console.log(active);
+      tabs.forEach((el) => el.classList.remove(('is-active')));
+      this.classList.add('is-active');
+
+      tabContent.forEach((el) => el.classList.remove('is-active'));
+      active.classList.add('is-active');
+    });
+  });
+
   document.getElementById('startTimer').addEventListener('click', timer);
   document.getElementById('options').addEventListener('click', options);
 }
