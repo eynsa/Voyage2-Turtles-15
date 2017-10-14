8975568ef705ddc753c5d907f2a7e00dc3480cd0
Steven Eysermans <31731363+eynsa@users.noreply.github.com>
2017-10-14 16:21:15 +0200

    removed webRequestBlocker class and returned its functionality to background.js

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,7 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
+/** document init function */
+function init() {
   let params = new URLSearchParams(window.location.search);
   if (params.has('url')) {
     let subtitle = document.querySelector('h3.subtitle');
     subtitle.textContent = params.get('url');
   }
-});
+}
+
+document.addEventListener('DOMContentLoaded', init);
