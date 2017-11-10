7819c8949812f3707d002df00139b2f7157f6473
Steven Eysermans <31731363+eynsa@users.noreply.github.com>
2017-11-10 16:28:54 +0100

    installed styles-, css-, and sass- loaders for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
-const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/popup.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: {
+    popup: './src/popup.js',
+    vendors: './src/vendors.js',
   },
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/dist',
+  },
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+        }),
+      }],
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+  ],
 };
