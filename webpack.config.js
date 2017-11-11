ebc71c4053083570940bcc1ec62d6681e22503c9
Steven Eysermans <31731363+eynsa@users.noreply.github.com>
2017-11-11 20:30:08 +0100

    got font-awesome and bulma working in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,16 @@ module.exports = {
         use: ExtractTextPlugin.extract({
           use: ['css-loader', 'sass-loader'],
         }),
+      }, {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            limit: 10000,
+            name: '[name].[ext]',
+            outputPath: 'fonts/',
+          },
+        }],
       }],
   },
   plugins: [
